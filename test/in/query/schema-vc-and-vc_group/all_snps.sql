f7a8859b92d3dd5e14c36fb1c617b886c042a831
James Gleeson <james@innovativemedicine.ca>
2013-01-21 16:43:28 -0500

    fix test queries that broke because of schema change

--- test/in/query/schema-vc-and-vc_group/all_snps.sql
+++ test/in/query/schema-vc-and-vc_group/all_snps.sql
@@ -1,4 +1,5 @@
 -- Query all the snps in the database.
-select distinct chromosome, start_posn, allele1 from vc
+select distinct chromosome, start_posn, allele1 from vc_group, vc where vc.vc_group_id = vc_group.id
 union 
-select distinct chromosome, start_posn, allele2 from vc
+select distinct chromosome, start_posn, allele2 from vc_group, vc where vc.vc_group_id = vc_group.id
+
