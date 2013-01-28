71928ad414da5cdb131f73ac0e27e84c97a644c8
James Gleeson <james@innovativemedicine.ca>
2013-01-28 18:10:12 -0500

    merge config.huge.ini into mars config.ini

--- script/mysqlc/startup.sh
+++ script/mysqlc/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 set -e
-ndb_mgmd -f conf/config.ini --initial --configdir=`readlink -f conf/`
+ndb_mgmd -f `readlink -f conf/config.ini` --initial --configdir=`readlink -f conf/`
 ndbd -c localhost:1186
 ndbd -c localhost:1186
-mysqld --defaults-file=conf/my.cnf
+mysqld --defaults-file=`readlink -f conf/my.cnf`
